8329133bf92aee6360677a26d99fc188e8e47998
Pradipta Rakahartyanto <pradiptaraka5@gmail.com>
2021-09-27 20:32:41 +0700

    added fade transitions in level 3
    all of the level 3 should have faded transitions, and the Scene Switcher has been edited to have some extra things in there

--- Assets/SceneSwitcher.cs
+++ Assets/SceneSwitcher.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class SceneSwitcher : MonoBehaviour
 {
-    
+    public int index;
+    public string levelName;
+    public Image black;
+    public Animator anim;
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    IEnumerator Fading(){
+        anim.SetBool("fade", true);
+        yield return new WaitUntil(()=>black.color.a==1);
+        SceneManager.LoadScene(index);
     }
 }
