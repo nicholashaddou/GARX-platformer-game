7f97f23ba18cda25c2d3474461c9e3a3f6f780ea
papinegro <54536702+papinegro@users.noreply.github.com>
2021-12-21 15:38:49 +0700

    Added health bar (temp asset)

--- Assets/MobileHealthController2D.cs
+++ Assets/MobileHealthController2D.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MobileHealthController2D : MonoBehaviour
 {
@@ -9,6 +10,18 @@ public class MobileHealthController2D : MonoBehaviour
     private int index;
     public int level = 2;
     public int health = 100;
+
+    public Slider healthSlider;
+
+    public void setMaxHealth(int health)
+    {
+        healthSlider.maxValue = health;
+        healthSlider.value = health;
+    }
+    public void setPlayerHealth(int health)
+    {
+        healthSlider.value = health;
+    }
 
     //for save/load
     /*private int passIndexValue()
